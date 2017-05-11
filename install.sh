5e16e37e80e2a779beb10c2c266af5f7f24a6962
Jadiel <jadielam@gmail.com>
2017-05-11 16:25:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo yum -y install python-devel
 sudo yum -y install libtiff-devel libjpeg-devel libzip-devel freetype-devel lcms2-devel libwebp-devel tcl-devel
 sudo pip install scikit-image
 git clone https://github.com/opencv/opencv.git
-cd ~/opencv
+cd opencv
 mkdir release
 cd release
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
