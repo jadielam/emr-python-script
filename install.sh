f23bbfb076d3fd6bed4888d010b371a1f0c354ab
Jadiel <jadielam@gmail.com>
2017-05-11 18:28:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,15 @@ sudo yum -y install cmake
 sudo yum -y install python-devel
 sudo yum -y install libtiff-devel libjpeg-devel libzip-devel freetype-devel lcms2-devel libwebp-devel tcl-devel
 sudo pip install scikit-image
+sudo pip install imageio
 git clone https://github.com/opencv/opencv.git
 cd opencv
 mkdir release
 cd release
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
 make
+sudo make install
+cd ../..
+sudo cp /usr/local/lib/python2.7/dist-packages/cv2.so /usr/lib/python2.7/site-packages
+
+
