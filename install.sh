b9097fea550bff10cbb3d01817a80bee2a2b95c7
Jadiel <jadielam@gmail.com>
2017-05-11 15:01:34 -0400

    Create install.sh
    Installs python needed libraries in AWS EMR cluster

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo pip install sklearn
+sudo pip install xgboost
+sudo yum -y install git
+sudo yum -y install cmake
+sudo yum -y install python-devel
+sudo yum -y install libtiff-devel libjpeg-devel libzip-devel freetype-devel lcms2-devel libwebp-devel tcl-devel
+sudo pip install scikit-image
+git clone https://github.com/opencv/opencv.git
+cd ~/opencv
+mkdir release
+cd release
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make
